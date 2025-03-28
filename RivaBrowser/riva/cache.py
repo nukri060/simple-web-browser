9e344b61393c591c994968138023d888806a75f9
nukri <127565043+nukri060@users.noreply.github.com>
2025-03-29 03:44:10 +0400

    [FEATURE] Add connection timeout tracking - Store timestamps with sockets - Automatic expiration of old connections - Add _remove_connection helper

--- RivaBrowser/riva/cache.py
+++ RivaBrowser/riva/cache.py
@@ -1,25 +1,33 @@
 import socket
+import time
 
 class ConnectionCache:
+    CONNECTION_TIMEOUT = 30  # seconds
+    
     def __init__(self):
-        # Stores open sockets: {(host, port, scheme): socket}
-        self.cache = {}
+        self.cache = {}  # {(host,port,scheme): (socket, timestamp)}
     
     def get(self, host, port, scheme):
-        # Unique identifier for each connection
         key = (host, port, scheme)
-        return self.cache.get(key) 
+        if key in self.cache:
+            sock, timestamp = self.cache[key]
+            if time.time() - timestamp < self.CONNECTION_TIMEOUT:
+                return sock
+            self._remove_connection(key)
+        return None
     
     def store(self, host, port, scheme, sock):
         key = (host, port, scheme)
-        self.cache[key] = sock # Save socket for future reuse
+        self.cache[key] = (sock, time.time())
     
-    def close_all(self):
-        for sock in self.cache.values():
+    def _remove_connection(self, key):
+        sock, _ = self.cache.pop(key, (None, None))
+        if sock:
             try:
                 sock.close()
             except:
                 pass
-        self.cache.clear()
-
-connection_cache = ConnectionCache()
+    
+    def close_all(self):
+        for key in list(self.cache.keys()):
+            self._remove_connection(key)
